44862d88ece946ceb01739649b744895570a9bcb
JMojicaR <jrodmoj@gmail.com>
2025-02-23 16:28:14 -0600

    Variables and functions

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 //import "fmt"
-import "github.com/goCourse/variables"
+import (
+	"fmt"
+
+	"github.com/goCourse/variables"
+)
 
 func main() {
 	variables.MuestraEnteras()
+	variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(2345)
+	fmt.Println("Estado = ", estado)
+	fmt.Println("Texto = ", texto)
 }
--- variables/enteras.go
+++ variables/enteras.go
@@ -3,8 +3,8 @@ package variables
 import "fmt"
 
 func MuestraEnteras() {
-	var intcomun int
-	intde32 := int32(10)
+	var intcomun int     //variable por declaracion
+	intde32 := int32(10) //variable por asignacion
 	intde64 := int64(100)
 	fmt.Println("intcomun = ", intcomun)
 	fmt.Println("intde32 = ", intde32)
